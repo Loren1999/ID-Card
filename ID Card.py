112a5a16606ef7a1a9ead0108784a6faa135e211
LonLoc <1513049385@qq.com>
2019-05-16 11:30:11 +0800

    Add files via upload

---
+++ ID Card.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+
+import time
+
+
+def id_card():
+    # 身份证号 4210871986****271X
+    # 姓名 陈元
+
+    s = "4210871986****271X"
+
+    # 根据指定的格式把一个时间字符串解析为时间元组
+    first_strp_time = time.strptime(s[6:10] + '0101', '%Y%m%d')
+    last_strp_time = time.strptime(s[6:10] + '1231', '%Y%m%d')
+    # 返回用秒数来表示时间的浮点数,取整后得到Unix时间戳
+    first_mk_time = int(time.mktime(first_strp_time))
+    last_mk_time = int(time.mktime(last_strp_time)) + 1
+    sfz = [
+        s.replace('****', j[4:])
+        for j in [time.strftime('%Y%m%d', time.localtime(i)) for i in range(first_mk_time, last_mk_time, 3600 * 24)]
+        if s[-1] == '10X98765432'
+        [sum(map(lambda x: int(x[0]) * x[1], zip(s.replace('****', j[4:]), [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]))) % 11]
+    ]
+
+    for i in sfz:
+        print(i)
+
+
+if __name__ == '__main__':
+    id_card()
